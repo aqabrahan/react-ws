baa734aed3bb1e9edfc484cc53f99cee1e73cddd
Abrahan <abrahan.apaza@mercadolibre.com>
2019-11-12 03:11:30 -0300

    add prod version

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
 import React, { Component, Suspense, lazy } from 'react';
 import './App.css';
 //import Comp from './components/suspense';
-//const Comp = React.lazy(() => import('./components/suspense'));
+//const Comp = lazy(() => import('./components/suspense'));
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import ErrorBoundary from './components/suspense/errorBoundary';
+//import ErrorBoundary from './components/suspense/errorBoundary';
+//import Loading from './components/suspense/loading';
 
-/* import Home from './components/suspense/home';
+import Home from './components/suspense/home';
 import Graph from './components/suspense/graph';
-import Images from './components/suspense/images'; */
-const Home = lazy(() => import('./components/suspense/home'));
+import Images from './components/suspense/images';
+/* const Home = lazy(() => import('./components/suspense/home'));
 const Graph = lazy(() => import('./components/suspense/graph'));
-const Images = lazy(() => import('./components/suspense/images'));
+const Images = lazy(() => import('./components/suspense/images')); */
 class App extends Component {
   render() {
     return (
       <div className="context">
           <Router>
-            <ErrorBoundary>
-            <Suspense fallback={<div className="spinner-border" role="status"><span className="sr-only">Loading...</span></div>}>
               <Switch>
                 <Route exact path="/" component={Home} />
                 <Route path="/graph" component={Graph} />
                 <Route path="/images" component={Images} />
               </Switch>
-              </Suspense>
-              </ErrorBoundary>
           </Router>
       </div>
     );
