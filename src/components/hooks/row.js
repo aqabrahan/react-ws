91b38c072252fd655c0d8031d20f0cd9925b6411
Abrahan <abrahan.apaza@mercadolibre.com>
2019-11-13 01:20:08 -0300

    custom hooks

--- src/components/hooks/row.js
+++ src/components/hooks/row.js
@@ -2,33 +2,13 @@ import React, { useState, useContext, useEffect } from 'react'
 import { ThemeContext, LangContext } from './context';
 
 export default function Row(props) {
-  const [name, setName] = useState('Son')
-  const [lastname, setLastname] = useState('Gohan')
-  const [width, setWidth] = useState(window.innerWidth);
-
+  const name = useFormInput('Son');
+  const lastname = useFormInput('Gohan');
+  const width = useWindowWith();
   const theme = useContext(ThemeContext);
   const lang = useContext(LangContext);
 
-  const onChangeName = (e) => {
-    setName(e.target.value);
-  }
-  const onChangeLastname = (e) => {
-    setLastname(e.target.value);
-  }
-
-  useEffect(() => {
-    document.title = name + ' ' + lastname;
-  });
-
-  useEffect(() => {
-    const resizeWindow = () => setWidth(window.innerWidth);
-    window.addEventListener('resize', resizeWindow);
-    return () => {
-      window.removeEventListener('resize', resizeWindow);
-    }
-  });
-
-
+  useDocument(name.value + ' ' + lastname.value);
 
   return (
     <>
@@ -37,13 +17,13 @@ export default function Row(props) {
       </div>
       <div className={`alert alert-${theme}`}>
         <p><strong>Name: </strong></p>
-        <p>{name}</p>
-        <input value={name} onChange={onChangeName} />
+        <p>{name.value}</p>
+        <input {...name} />
       </div>
       <div className={`alert alert-${theme}`}>
         <p><strong>LastName: </strong></p>
-        <p>{lastname}</p>
-        <input value={lastname} onChange={onChangeLastname} />
+        <p>{lastname.value}</p>
+        <input {...lastname} />
       </div>
       <div className="jumbotron">
         you speak: <strong>{lang}</strong>
@@ -53,4 +33,34 @@ export default function Row(props) {
       </div>
     </>
   )
+}
+
+function useFormInput(initValue) {
+  const [value, setValue] = useState(initValue);
+  function handlerChange(e) {
+    setValue(e.target.value);
+  }
+
+  return {
+    value,
+    onChange: handlerChange
+  }
+}
+
+function useDocument(title) {
+  useEffect(() => {
+    document.title = title;
+  });
+}
+
+function useWindowWith() {
+  const [width, setWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    const resizeWindow = () => setWidth(window.innerWidth);
+    window.addEventListener('resize', resizeWindow);
+    return () => {
+      window.removeEventListener('resize', resizeWindow);
+    }
+  });
+  return width;
 }
