b8cc0af31276b0b65d4a3bff38a6de9fbfd1607f
Abrahan <abrahan.apaza@mercadolibre.com>
2019-11-13 02:15:08 -0300

    reducer

--- src/components/hooks/row.js
+++ src/components/hooks/row.js
@@ -1,13 +1,20 @@
-import React, { useState, useContext, useEffect } from 'react'
+import React, { useState, useContext, useEffect, useReducer } from 'react'
 import { ThemeContext, LangContext } from './context';
-
+const initialState = {
+  tasks: [
+    { id: 1, name: "Go Office", done: false },
+    { id: 2, name: "On Vacation", done: false },
+    { id: 3, name: "Send email", done: false },
+    { id: 4, name: "Go to Gym", price: false }
+  ]
+};
 export default function Row(props) {
   const name = useFormInput('Son');
   const lastname = useFormInput('Gohan');
   const width = useWindowWith();
   const theme = useContext(ThemeContext);
   const lang = useContext(LangContext);
-
+  const [state, dispatch] = useReducer(reducer, initialState);
   useDocument(name.value + ' ' + lastname.value);
 
   return (
@@ -24,6 +31,20 @@ export default function Row(props) {
         <p><strong>LastName: </strong></p>
         <p>{lastname.value}</p>
         <input {...lastname} />
+      </div>
+      <div className="jumbotron">
+        <h2>useReducer Tasks</h2>
+        <ul className="list-group">
+          {state.tasks.map((t, k) => {
+            return <li key={k} className="list-group-item">
+              <span className={`${t.done ? 'done' :''}`}>{t.name}</span>
+              {' '}
+              <button onClick={() => dispatch({type: 'done', id: t.id})} className="btn btn-danger btn-sm pull-right">Done</button>
+              <button onClick={() => dispatch({type: 'delete', id: t.id})} className="btn btn-danger btn-sm pull-right">Delete</button>
+            </li>
+          })}
+
+        </ul>
       </div>
       <div className="jumbotron">
         you speak: <strong>{lang}</strong>
@@ -63,4 +84,30 @@ function useWindowWith() {
     }
   });
   return width;
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'done':
+      return {
+        ...state,
+        tasks: state.tasks.map((v, i) => {
+          if (v.id === action.id) {
+            return {
+              ...v,
+              done: true
+            }
+          } else {
+            return v
+          }
+        })
+      };
+    case 'delete':
+      return {
+        ...state,
+        tasks: state.tasks.filter(t => t.id !== action.id)
+      };
+    default:
+      throw new Error();
+  }
 }
